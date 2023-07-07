8d8cb657edd77b6d49aeded27de57e852c13ea52
cicekhasann <72624047+cicekhasann@users.noreply.github.com>
2023-07-07 14:43:07 +0300

    fix the run loop

--- VolumeController.py
+++ VolumeController.py
@@ -6,6 +6,7 @@ import keyboard
 import time
 from HandDetector import HandDetector
 
+
 class VolumeController:
     def __init__(self):
         self.webcamFeed = None
@@ -32,49 +33,51 @@ class VolumeController:
             return alsaaudio.Mixer()
 
     def run(self):
-        is_active = False
-        start_time = 0
+        while True:
+            if keyboard.is_pressed('shift+f4'):  # if keyboard.is_pressed('shift') and keyboard.is_pressed('f4'):
+                self.process_volume_control()
+
+            if keyboard.is_pressed('ctrl+q'):
+                break
+
+    def process_volume_control(self):
+        webcamFeed = cv2.VideoCapture(0)
+        start_time = time.time()
 
         while True:
-            if keyboard.is_pressed('ctrl+f4'): #if keyboard.is_pressed('shift') and keyboard.is_pressed('capslock'):
-                if not is_active:
-                    is_active = True
-                    self.webcamFeed = cv2.VideoCapture(0)
-                    start_time = time.time()
-            elif keyboard.is_pressed('ctrl+q'):
-                if is_active:
-                    is_active = False
-                    self.webcamFeed.release()
-                    self.webcamFeed = None
+            status, image = webcamFeed.read()
+            image = cv2.flip(image, 1)
 
-            if is_active:
-                if self.webcamFeed is not None:
-                    status, image = self.webcamFeed.read()
-                    image = cv2.flip(image, 1)
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 5:
+                webcamFeed.release()
+                break
 
-                    elapsed_time = time.time() - start_time
-                    if elapsed_time >= 5:
-                        self.webcamFeed.release()
-                        self.webcamFeed = None
+            handLandmarks = self.handDetector.findHandLandMarks(image=image, draw=True)
 
-                    if self.webcamFeed is not None:
-                        handLandmarks = self.handDetector.findHandLandMarks(image=image, draw=True)
+            if len(handLandmarks) != 0:
+                x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
+                x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
+                length = math.hypot(x2 - x1, y2 - y1)
+                print(length)
 
-                        if len(handLandmarks) != 0:
-                            x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
-                            x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
-                            length = math.hypot(x2 - x1, y2 - y1)
-                            print(length)
+                # Hand range(length): 50-250
+                # Volume Range: (-65.25, 0.0) for Windows, 0-100 for Linux
 
-                            # Hand range(length): 50-250
-                            # Volume Range: (-65.25, 0.0) for Windows, 0-100 for Linux
+                if platform.system() == 'Windows':
+                    volumeValue = np.interp(length, [50, 250],
+                                            [-65.25, 0.0])  # converting length to proportionate to volume range
+                    self.volume_control.SetMasterVolumeLevel(volumeValue, None)
+                elif platform.system() == 'Linux':
+                    volumeValue = np.interp(length, [50, 250],
+                                            [0, 100])  # converting length to proportionate to volume range
+                    self.set_system_volume(volumeValue)
 
-                            if platform.system() == 'Windows':
-                                volumeValue = np.interp(length, [50, 250], [-65.25, 0.0])  # converting length to proportionate to volume range
-                                self.volume_control.SetMasterVolumeLevel(volumeValue, None)
-                            elif platform.system() == 'Linux':
-                                volumeValue = np.interp(length, [50, 250], [0, 100])  # converting length to proportionate to volume range
-                                self.set_system_volume(volumeValue)
+            cv2.imshow("Volume", image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
 
     def set_system_volume(self, volume_value):
         self.volume_control.setvolume(int(volume_value))
