49fe187a1aed4a6c431ca0045fcf48a4c8edaaf7
Muzaffer-Irmak-Yavuz <muzafferirmaky@gmail.com>
2023-07-07 09:25:38 +0200

    Added if main idiom

--- VolumeController.py
+++ VolumeController.py
@@ -18,28 +18,32 @@ volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 # TODO: Seperate VolumeController.py from main.py
 
-while True:
-    status, image = webcamFeed.read()
-    handLandmarks = handDetector.findHandLandMarks(image=image, draw=True)
+def main():
+    while True:
+        status, image = webcamFeed.read()
+        handLandmarks = handDetector.findHandLandMarks(image=image, draw=True)
 
-    if(len(handLandmarks) != 0):
-        #for volume control we need 4th and 8th landmark
-        #details: https://google.github.io/mediapipe/solutions/hands
-        x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
-        x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
-        length = math.hypot(x2-x1, y2-y1)
-        print(length)
+        if(len(handLandmarks) != 0):
+            #for volume control we need 4th and 8th landmark
+            #details: https://google.github.io/mediapipe/solutions/hands
+            x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
+            x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
+            length = math.hypot(x2-x1, y2-y1)
+            print(length)
 
-        #Hand range(length): 50-250
-        #Volume Range: (-65.25, 0.0)
+            #Hand range(length): 50-250
+            #Volume Range: (-65.25, 0.0)
 
-        volumeValue = np.interp(length, [50, 250], [-65.25, 0.0]) #coverting length to proportionate to volume range
-        volume.SetMasterVolumeLevel(volumeValue, None)
+            volumeValue = np.interp(length, [50, 250], [-65.25, 0.0]) #coverting length to proportionate to volume range
+            volume.SetMasterVolumeLevel(volumeValue, None)
 
 
-        cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(image, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(image, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    cv2.imshow("Volume", image)
-    cv2.waitKey(1)
+        cv2.imshow("Volume", image)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
