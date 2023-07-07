45c0c1aa6e0e341607306ba169310dd7b556ee34
cicekhasann <72624047+cicekhasann@users.noreply.github.com>
2023-07-07 14:11:55 +0300

    The camera window does not open, and the program remains pending in the background. With CTRL+F4 keys, the camera works with 5 seconds and VolumControl is done. CTRL+Q closes the program.

--- VolumeController.py
+++ VolumeController.py
@@ -2,11 +2,13 @@ import cv2
 import math
 import numpy as np
 import platform
+import keyboard
+import time
 from HandDetector import HandDetector
 
 class VolumeController:
-    def __init__(self, webcam):
-        self.webcamFeed = webcam
+    def __init__(self):
+        self.webcamFeed = None
         self.handDetector = HandDetector(min_detection_confidence=0.7)
         self.volume_control = self.initialize_volume_control()
 
@@ -30,38 +32,49 @@ class VolumeController:
             return alsaaudio.Mixer()
 
     def run(self):
+        is_active = False
+        start_time = 0
+
         while True:
-            status, image = self.webcamFeed.read()
-            image = cv2.flip(image, 1)
-            handLandmarks = self.handDetector.findHandLandMarks(image=image, draw=True)
+            if keyboard.is_pressed('ctrl+f4'): #if keyboard.is_pressed('shift') and keyboard.is_pressed('capslock'):
+                if not is_active:
+                    is_active = True
+                    self.webcamFeed = cv2.VideoCapture(0)
+                    start_time = time.time()
+            elif keyboard.is_pressed('ctrl+q'):
+                if is_active:
+                    is_active = False
+                    self.webcamFeed.release()
+                    self.webcamFeed = None
 
-            if len(handLandmarks) != 0:
-                x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
-                x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
-                length = math.hypot(x2 - x1, y2 - y1)
-                print(length)
+            if is_active:
+                if self.webcamFeed is not None:
+                    status, image = self.webcamFeed.read()
+                    image = cv2.flip(image, 1)
 
-                # Hand range(length): 50-250
-                # Volume Range: (-65.25, 0.0) for Windows, 0-100 for Linux
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time >= 5:
+                        self.webcamFeed.release()
+                        self.webcamFeed = None
 
-                if platform.system() == 'Windows':
-                    volumeValue = np.interp(length, [50, 250], [-65.25, 0.0])  # converting length to proportionate to volume range
-                    self.volume_control.SetMasterVolumeLevel(volumeValue, None)
-                elif platform.system() == 'Linux':
-                    volumeValue = np.interp(length, [50, 250], [0, 100])  # converting length to proportionate to volume range
-                    self.set_system_volume(volumeValue)
+                    if self.webcamFeed is not None:
+                        handLandmarks = self.handDetector.findHandLandMarks(image=image, draw=True)
 
-                cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-                cv2.circle(image, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-                cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                        if len(handLandmarks) != 0:
+                            x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
+                            x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
+                            length = math.hypot(x2 - x1, y2 - y1)
+                            print(length)
 
-            cv2.imshow("Volume", image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                            # Hand range(length): 50-250
+                            # Volume Range: (-65.25, 0.0) for Windows, 0-100 for Linux
 
-        self.webcamFeed.release()
-        cv2.destroyAllWindows()
+                            if platform.system() == 'Windows':
+                                volumeValue = np.interp(length, [50, 250], [-65.25, 0.0])  # converting length to proportionate to volume range
+                                self.volume_control.SetMasterVolumeLevel(volumeValue, None)
+                            elif platform.system() == 'Linux':
+                                volumeValue = np.interp(length, [50, 250], [0, 100])  # converting length to proportionate to volume range
+                                self.set_system_volume(volumeValue)
 
     def set_system_volume(self, volume_value):
         self.volume_control.setvolume(int(volume_value))
-
--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import cv2
 from VolumeController import VolumeController
 
 def main():
-    webcamFeed = cv2.VideoCapture(0)
-    volumeController = VolumeController(webcamFeed)
+    volumeController = VolumeController()
     volumeController.run()
 
 if __name__ == "__main__":
